a41992ec6b06661215075554f0eb2a0fb1b23d3d
Elias <elias0789@gmail.com>
2016-04-04 01:44:23 +0100

    Fix up entry point to actually work

--- index.js
+++ index.js
@@ -1,57 +1,39 @@
-/*
-* Autoloader
-*
-* API:
-* const autoloader = require('autoloader')
-*
-* Default behaviour:
-* const modulesArray = autoloader.load(__dirname, excludes)
-* // or
-* const modulesArray = autoloader.load({
-*   dir: __dirname,
-*   include: [...patterns],
-*   exclude: [...patterns],
-*   global: (false* | true),
-*   output: ('array'* | 'object'),
-* })
-*
-* Object output mirrors directory structure:
-* ./
-* |- app
-* |- |- controllers
-* |- |- |- IndexController
-* |- |- |- |- index.js
-* gives app.controllers.IndexController, while
-* ./
-* |- app
-* |- |- controllers
-* |- |- |- IndexController.js
-* gives app.controllers.IndexController as well
-*/
 'use strict'
 
-const fs = require('fs')
+// TODO: support arrays for `dir`, `include`, `exclude`, `stopfile` options
+// TODO: support regex for `stopfile` option
 const path = require('path')
+const tree = require('./lib/tree.js')
+const obj = require('./lib/outils.js')
 
-export default function (dirname, excludes) {
-  console.log('[AUTOLOAD] Autoloading files from ' + dirname)
+const defaults = require('./lib/defaults.js')
+const validators = require('./lib/validators.js')
 
-  return recRead(dirname, excludes, true)
-          .filter((file) => typeof file !== 'undefined')
-          .map((file) => require(file))
-}
+const parser = obj.parser(defaults, validators)
 
-function recRead (dirname, excludes, isRoot) {
-  return fs.readdirSync.map((file) => {
-    if (file in excludes) {
-      console.log('[AUTOLOAD] Excluding ' + file + ' from loading')
-      return undefined
-    } else if (file.indexOf('.js') > -1 && (isRoot || file === 'index.js')) {
-      return path.join(dirname, file)
-    } else if (fs.isdirSync(file)) { // ? double check
-      return recRead(path.join(dirname, file), excludes, false)
-    } else {
-      return undefined
-    }
-  })
+module.exports = function (options) {
+  const opts = parser(options)
+
+  let files = tree.walk(opts.dir, null, opts.exclude, opts.include, opts.stopfile)
+
+  files = files.filter((file) => file.indexOf('.js') > -1)
+
+  let modules = files.map(require)
+
+  if (opts.output === 'array') return modules
+
+  let parsedObj = files
+    .map((file) => file.slice(opts.dir.length + 1))
+    .map((file) => {
+      const fobj = path.parse(file)
+      return (fobj.base === opts.stopfile) ? fobj.dir : file
+    })
+    .map((file, idx) => obj.fromFilePath(file, modules[idx]))
+    .reduce((acc, curr) => obj.merge(acc, curr), {})
+
+  if (opts.isglobal) {
+    global[opts.namespace] = parsedObj
+  } else {
+    return parsedObj
+  }
 }
