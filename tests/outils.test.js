dab518c82e39c9bb1f335e54396eb5aef3af3638
Elias <elias0789@gmail.com>
2016-04-11 19:10:51 +0100

    Update to use new utils export structure

--- tests/outils.test.js
+++ tests/outils.test.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const tape = require('tape')
-const obj = require('../lib/outils.js')
+const path = require('path')
+const utils = require('../lib/utils.js')
 
 tape('Empty filepath gives empty object', (t) => {
-  t.equal(Object.keys(obj.fromFilePath('')).length, 0)
+  t.equal(Object.keys(utils.object.createNested.fromString('')).length, 0)
   t.end()
 })
 
@@ -12,7 +13,7 @@ tape('Filepath with depth 1', (t) => {
   const expected = {
     index: 1
   }
-  t.deepEqual(obj.fromFilePath('index.js', 1), expected)
+  t.deepEqual(utils.object.createNested.fromString('index', path.sep, 1), expected)
   t.end()
 })
 
@@ -22,7 +23,7 @@ tape('Filepath with depth 2', (t) => {
       index: 1
     }
   }
-  t.deepEqual(obj.fromFilePath('foo/index.js', 1), expected)
+  t.deepEqual(utils.object.createNested.fromString('foo/index', path.sep, 1), expected)
   t.end()
 })
 
@@ -34,7 +35,7 @@ tape('Filepath with depth 3', (t) => {
       }
     }
   }
-  t.deepEqual(obj.fromFilePath('foo/bar/index.html', 1), expected)
+  t.deepEqual(utils.object.createNested.fromString('foo/bar/index', path.sep, 1), expected)
   t.end()
 })
 
@@ -43,7 +44,7 @@ tape('Object merge', (t) => {
   let b = {x: 2, y: {q: {r: 2, s: 1}}}
 
   let exp = {x: 2, y: {z: 2, q: {r: 2, s: 1}}}
-  let actual = obj.merge(a, b)
+  let actual = utils.object.merge(a, b)
 
   t.deepEqual(actual, exp, 'Merges')
   t.end()
